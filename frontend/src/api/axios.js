33c60a70e0ec238bca52f17dfd8d8446d8434927
kartikjaiswal99 <kartik99jais@gmail.com>
2025-09-04 22:04:17 +0530

    backend URL changed

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8000', 
+  // baseURL: 'http://localhost:8000', 
+  baseURL: 'https://kartik.micro-scale.software/flowstack', 
   headers: {
     'Content-Type': 'application/json',
   },
